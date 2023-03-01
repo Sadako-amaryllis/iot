1165dcd696ad2bc1d1a93348b9d549cdb5f5beab
Mylene <mylene.mzali@edu.devinci.fr>
2023-03-01 16:47:57 +0100

    adding python iot without ssid & password

---
+++ main.py
@@ -0,0 +1,17 @@
+from machine import I2C, Pin
+from pico_i2c_lcd import I2cLcd
+import utime
+
+i2c = I2C(0, sda=Pin(8), scl=Pin(9), freq=400000)
+print(i2c.scan())
+utime.sleep(1)
+I2C_ADDR = i2c.scan()[0]
+
+lcd = I2cLcd(i2c, I2C_ADDR, 2, 16)
+while True:
+  print(I2C_ADDR, "| Hex:",hex(I2C_ADDR))
+  print()
+  lcd.move_to(0,0)
+  lcd.putstr("I2CAddress:"+hex(I2C_ADDR)+"\n")
+  lcd.move_to(0,1)
+  lcd.putstr("klm")
